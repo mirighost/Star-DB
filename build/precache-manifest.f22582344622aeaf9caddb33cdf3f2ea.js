ff66e14659523f31036b1cfc28f1a1fdd47c3fc7
Mirhasan Huseynzade <mirhasan123321@gmail.com>
2020-03-18 13:37:53 +0400

    Done

---
+++ build/precache-manifest.f22582344622aeaf9caddb33cdf3f2ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee0a891bb5b671e6cf6b4ca60e431bdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d74e5b0d1a9cadb1505",
+    "url": "/static/css/main.b1736cf5.chunk.css"
+  },
+  {
+    "revision": "23c83f9c7dbd2c774445",
+    "url": "/static/js/2.5761d576.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.5761d576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d74e5b0d1a9cadb1505",
+    "url": "/static/js/main.5935d8e1.chunk.js"
+  },
+  {
+    "revision": "0a9ed1ed035e91451a23",
+    "url": "/static/js/runtime-main.a407ddaa.js"
+  },
+  {
+    "revision": "eed4945c262e86a90062b19c5590ffbe",
+    "url": "/static/media/error.eed4945c.png"
+  }
+]);
